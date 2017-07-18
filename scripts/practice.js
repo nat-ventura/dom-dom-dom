cbc0bde2fb9e362c3cc5d5068bc8dbc96574aa7b
Natalie Villasana <navillasa@gmail.com>
2017-07-18 11:34:54 -0400

    followed chris's solution and added each function

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,12 +1,62 @@
-var thumbnailNodeList = document.querySelectorAll('.thumbnail-item');
+var THUMBNAIL_SELECTOR_STRING = "[data-image-role='trigger']";
+var TARGET_SELECTOR_STRING = "[data-image-role='target']";
+var TITLE_SELECTOR_STRING = "[data-image-role='title']";
 
-// converts thumbnail nodeList into array
-function toArray(thumbnailNodeList) {
-    var arr = thumbnailNodeList;
-    if (thumbnailNodeList.forEach === undefined) {
-        arr = [].slice.call(thumbnailNodeList);
-    }
-    return arr;
+function thumbImage(thumbnail) {
+    return thumbnail.getAttribute('data-image-url');
 }
 
-console.log(toArray(thumbnailNodeList));
+function thumbTitle(thumbnail) {
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    setDetails(thumbImage(thumbnail), thumbTitle(thumbnail));
+}
+
+function thumbClicker(thumb) {
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+    });
+}
+
+function makeThumbArray() {
+    var thumbnails = document.querySelectorAll(THUMBNAIL_SELECTOR_STRING);
+    var thumbArray = [].slice.call(thumbnails);
+    // it's okay to make this an anonymous function because
+    // it has such a specific use that you only use once
+    return thumbArray;
+}
+
+function main() {
+    var thumbnails = makeThumbArray();
+    thumbnails.forEach(thumbClicker);
+}
+
+main();
+
+// // converts thumbnail nodeList into array
+// function toArray(nodelist) {
+//     var arr = nodelist;
+//     if (nodelist.forEach === undefined) {
+//         arr = [].slice.call(nodelist);
+//     }
+//     return arr;
+// }
+
+// var thumbnailList = toArray(THUMBNAIL_LINK_SELECTOR);
+
+// var target = document.querySelector(TARGET_SELECTOR_STRING);
+// thumbnailList.forEach(function (thumbNav) {
+//     thumbNav.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         target.setAttribute('src', thumbNav.getAttribute('href'));
+//     });
+// });
+
+// var trigger = document.querySelector(TRIGGER_SELECTOR_STRING);
+
+
+
+// console.log(thumbnailList);
